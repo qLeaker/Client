24c61139fc948686a8b14c62afb8a54448bc89c5
Neki_play <neci.03@mail.ru>
2023-09-16 05:11:18 +0500

    Resources fix

--- form.py
+++ form.py
@@ -18,6 +18,7 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
 from PySide6.QtWidgets import (QApplication, QFrame, QLineEdit, QListWidget,
     QListWidgetItem, QMainWindow, QPushButton, QSizePolicy,
     QTabWidget, QWidget)
+import resources
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
